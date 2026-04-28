dd7a642fd3e7fe71187ef24d76a65ba9238e9338
Jowin Yip <jowin.yip@gmail.com>
2026-04-28 23:55:32 +0800

    fix: resolve worker and store typing errors; apply prettier formatting

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 
-export const randomIdeaArrayStore = writable([]);
-export const randomProgressArrayStore = writable([]);
+export const randomIdeaArrayStore = writable(/** @type {string[]} */ ([]));
+export const randomProgressArrayStore = writable(/** @type {string[]} */ ([]));
--- static/timerWorker.js
+++ static/timerWorker.js
@@ -1,31 +1,28 @@
 self.addEventListener('message', function (event) {
-    if (event.data.action === 'start') {
-        startTimer(event.data.timer);
-    }
-    else if (event.data.action === 'stop') {
-        clearInterval(self.timerInterval);
-    }
-    else if (event.data.action === 'addTime') {
-        addTime(event.data.additionalTime);
-    }
+	if (event.data.action === 'start') {
+		startTimer(event.data.timer);
+	} else if (event.data.action === 'stop') {
+		clearInterval(self.timerInterval);
+	} else if (event.data.action === 'addTime') {
+		addTime(event.data.additionalTime);
+	}
 });
 function startTimer(timer) {
-    self.currentTimer = timer;
-    self.timerInterval = setInterval(function () {
-        if (self.currentTimer > 0) {
-            self.currentTimer--;
-            self.postMessage({ message: 'update', timer: self.currentTimer });
-            if (self.currentTimer === 60) {
-                self.postMessage({ message: 'One minute left' });
-            }
-        }
-        else {
-            clearInterval(self.timerInterval);
-            self.postMessage({ message: 'Time is up' });
-        }
-    }, 1000);
+	self.currentTimer = timer;
+	self.timerInterval = setInterval(function () {
+		if (self.currentTimer > 0) {
+			self.currentTimer--;
+			self.postMessage({ message: 'update', timer: self.currentTimer });
+			if (self.currentTimer === 60) {
+				self.postMessage({ message: 'One minute left' });
+			}
+		} else {
+			clearInterval(self.timerInterval);
+			self.postMessage({ message: 'Time is up' });
+		}
+	}, 1000);
 }
 function addTime(additionalTime) {
-    self.currentTimer += additionalTime;
-    self.postMessage({ message: 'update', timer: self.currentTimer });
+	self.currentTimer += additionalTime;
+	self.postMessage({ message: 'update', timer: self.currentTimer });
 }
